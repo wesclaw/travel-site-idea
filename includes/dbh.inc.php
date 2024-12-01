2f93c4a9baf545fa6f1df714a3b67f4234f8bef3
Wesley Knudson <108541397+wesclaw@users.noreply.github.com>
2024-12-01 22:34:50 +0100

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -20,19 +20,3 @@ if ($conn->connect_error) {
 
 
 
-
-
-
-
-// $servername = "localhost";  
-// $username = "root";  
-// $password = "";  
-// $dbname = "travel_co";
-
-// // Create a new MySQLi connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-
-// // Check connection
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
